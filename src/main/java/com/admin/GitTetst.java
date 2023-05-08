0696f6a118df3b74adbea12e5413b507c75d1b19
Zz <zhangzai@swordgate.cn>
2023-05-08 17:48:34 +0800

    third commit

--- src/main/java/com/admin/GitTetst.java
+++ src/main/java/com/admin/GitTetst.java
@@ -12,5 +12,7 @@ package com.admin;
 public class GitTetst {
 	public static void main (String[] args) {
 		System.out.println( "hello git!" );
+		System.out.println( "hello git!2" );
+		System.out.println( "hello git!3" );
 	}
 }
