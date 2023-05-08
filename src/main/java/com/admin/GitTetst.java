4219bdff4a8999dc274ed926c67465e559e59adf
Zz <zhangzai@swordgate.cn>
2023-05-08 21:03:18 +0800

    gitee test

--- src/main/java/com/admin/GitTetst.java
+++ src/main/java/com/admin/GitTetst.java
@@ -19,6 +19,7 @@ public class GitTetst {
 		System.out.println( "hot-fix test!" );
 		System.out.println( "push test" );
 		System.out.println( "pull test" );
+		System.out.println( "gitee test!" );
 
 	}
 }
