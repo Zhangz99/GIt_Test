bb216ed1847f03729e3b1ed32b04611b6c28f468
Zz <zhangzai@swordgate.cn>
2023-05-08 20:04:42 +0800

    push commit

--- src/main/java/com/admin/GitTetst.java
+++ src/main/java/com/admin/GitTetst.java
@@ -17,6 +17,7 @@ public class GitTetst {
 		System.out.println( "hello git!4" );
 		System.out.println( "master test" );
 		System.out.println( "hot-fix test!" );
+		System.out.println( "push test" );
 
 	}
 }
